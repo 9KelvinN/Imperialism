863221f1a5265cfd63dcb0af9f066c5046b4be40
vlad <vladov3000@gmail.com>
2022-10-22 12:38:15 -0700

    added networking

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
+const express = require('express');
 
+const app = express();
+app.use(express.static('public'));
 
+const server = require('http').createServer(app);
+const io     = require('socket.io')(server);
 
-console.log('hello world')
+io.on("connection", (socket) => {
+		// send a message to the client
+		socket.emit("hello from server", { whatever: "some data" });
+
+		// receive a message from the client
+		socket.on("hello from client", (...args) => {
+				console.log(`got hello from client ${args}`);
+		});
+});
+
+server.listen(3000);
